cb333ac2137ba228c1399c9562e7077c1ef0497a
binkie-mudd <babykimminji@gmail.com>
2026-06-10 11:29:27 +0700

    Add files via upload

---
+++ custom-chatbox.js
@@ -0,0 +1,8 @@
+document.addEventListener('onLoad', function(obj) {
+  // widget loaded
+});
+
+document.addEventListener('onEventReceived', function(obj) {
+  // Streamlabs handles chat rendering via the template above
+  // This is here for any extra custom logic if needed
+});
